006f66516fd80b97cb9b4154e9eccce6b80ac13a
cmechelinck <chloe.mechelinck@insa-rouen.fr>
2021-12-06 12:59:37 +0100

    heuristique est quasi complète sauf pour les dames

--- Code/NoeudDame.java
+++ Code/NoeudDame.java
@@ -82,34 +82,47 @@ public class NoeudDame extends Noeud{
 		
 		//donne un score entier au coup réalisé
 		int tmp=0;
-		/*
-		//test si est une dame (
-		if (exemple.getCoordonnees().Y() ==0 ) {
-			tmp = tmp+15;}
-		//test position imprenable
-		if ((exemple.getCoordonnees().Y() ==0 )|(exemple.getCoordonnees().Y()== valeur.getTaille() )) {
-			tmp= tmp + 7;}
-		// à une pièce à proximité = reste vérifier si il peut manger
-		if (exemple.getCoordonnees().testexistance(valeur.getTaille(), exemple.getCoordonnees().X()+1,exemple.getCoordonnees().Y()+1)){
-			if (valeur.getPiece(exemple.getCoordonnees().X()+1,exemple.getCoordonnees().Y()+1).getCouleur() == exemple.getCouleur()){  //comparer couleur de la pièce présente sur cette case)
-				tmp = tmp + 5;
-			}
-		}
-				if (exemple.getCoordonnees().testexistance(valeur.getTaille(), exemple.getCoordonnees().X()+1,exemple.getCoordonnees().Y()-1)){
-					if (valeur.getPiece(exemple.getCoordonnees().X()+1,exemple.getCoordonnees().Y()-1).getCouleur() == exemple.getCouleur()){  //comparer couleur de la pièce présente sur cette case)
-					tmp = tmp + 5;
-			}
-		}*/
+		int x1=listeDeCoups.get(0).getPieceAvantD().getC().X();
+		int x2=listeDeCoups.get(0).getPieceApresD().getC().X();
+		int y1=listeDeCoups.get(0).getPieceAvantD().getC().Y();
+		int y2=listeDeCoups.get(0).getPieceApresD().getC().Y();
 		boolean tourBlanc;
-		if (listeDeCoups.get(0).getPieceAvantD().getCouleur()==Couleur.Blanc) {
-			tourBlanc=true;
-		}else {
-			tourBlanc=false;
+			if (listeDeCoups.get(0).getPieceAvantD().getCouleur()==Couleur.Blanc) {
+				tourBlanc=true;
+			}else {
+				tourBlanc=false;
+			}
+		
+		//faire une dame
+		if ((x2== 0)|(listeDeCoups.get(0).getPieceAvantD() instanceof Pion)){	
+			tmp=+26;
 		}
+		//perdre un pion
+		
+		//position imprenable
+		if (((x2== 0)|(listeDeCoups.get(0).getPieceAvantD() instanceof Reine))&&(y2==0)&&(y2==valeur.getTaille())){
+			tmp=+10;
+		 }
+		
+		//est en danger
+		if (listeDeCoups.get(0).getPieceApresD().peutEtreMange(tourBlanc, valeur.getTaille(), peutMangerEnArriere,obligerLesSauts)){  //ça me propose être manger c'est normal ? ça fait bien les choses ?
+			tmp=+15;
+			}else{
+				//if ((valeur.getCase(x2-1,y1-1).getPiece() == null)|( valeur.getCase(x2-1,y1-1).getPiece()==null)){
+				//tmp=+3;
+				//}
+			}
+		
+		//position neutre qui gagne quand même du terrain donc positif
+		
+		
+		
+		
 		//if (listeDeCoups.get(0).getPieceAvantD().sautPossible(tourBlanc, peutMangerEnArriere, true)&&(listeDeCoups.get(0).getPieceApresD().getC())) {
-		if (listeDeCoups.get(0).getPieceAvantD().sautPossible(tourBlanc, peutMangerEnArriere, true)) {
-			int x1=listeDeCoups.get(0).getPieceAvantD().getC().X();
-			int x2=listeDeCoups.get(0).getPieceApresD().getC().X();
+		
+		//pour manger
+	//	if (listeDeCoups.get(0).getPieceAvantD().sautPossible(tourBlanc, peutMangerEnArriere, true)) {
+			
 			if (abs(x1-x2)>=2) {
 				if (obligerLesSauts) {
 					tmp=+(listeDeCoups.size())*50000;
@@ -118,11 +131,14 @@ public class NoeudDame extends Noeud{
 				}
 			}
 			
-		}
+		
 		//faire aussi en cas de sauts multiples
+		System.out.println(tmp);
 	return(tmp);
 	}
+
 	
+
 	private int abs(int a) {
 		if (a>=0) {
 			return a;
@@ -131,4 +147,6 @@ public class NoeudDame extends Noeud{
 		}
 	}
 
+
 }
+
